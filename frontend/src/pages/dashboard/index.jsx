356165f802eccbfa90346b6602d839e0decf589b
AjinkyaLY <ajinkya.b@ly.design>
2023-12-26 18:37:28 +0530

    changes

--- frontend/src/pages/dashboard/index.jsx
+++ frontend/src/pages/dashboard/index.jsx
@@ -3,7 +3,7 @@ import { useResizeObserver } from '@wojtekmaj/react-hooks';
 import { pdfjs, Document, Page } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
-import PDF from '../../assets/AggrementPdf.pdf';
+import PDF from '../../assets/AgreementPdf.pdf';
 
 import './Sample.css';
 import PreviousNextButtons from '../../components/PreviousNextButtons';
